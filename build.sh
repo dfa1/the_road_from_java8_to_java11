0a1c70003c19933d24190daaad86476965b07221
Davide Angelocola <davide.angelocola@gmail.com>
2018-09-29 10:54:20 +0200

    improved build script

--- build.sh
+++ build.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+OUT_FILE=${1:-the_road_from_java8_to_java11.pdf}
 set -e
 bundle install
-asciidoctor-pdf                                  \
-  --failure-level WARN                           \
-  --attribute revnumber="$(git describe)"        \
-  --attribute revdate="$(date +%Y-%m-%d)"        \
-  main.adoc                                      \
-  --out-file 'the_road_from_java8_to_java11.pdf'
-
+asciidoctor-pdf                           \
+  --failure-level WARN                    \
+  --attribute revnumber="$(git describe)" \
+  --attribute revdate="$(date +%Y-%m-%d)" \
+  main.adoc                               \
+  --out-file ${OUT_FILE}
+echo ${OUT_FILE} successfully created.
