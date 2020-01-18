790b9bf7a8e55db52627f0984bcb6d64fce69540
Davide Angelocola <davide.angelocola@gmail.com>
2020-01-18 09:06:52 +0100

    fix indentation in snippet

--- snippets/java11_http_client.java
+++ snippets/java11_http_client.java
@@ -3,22 +3,21 @@ import java.net.http.*;
 import java.time.Duration;
 
 public class HttpClientDemo {
-		public static void main(String[] args) throws Exception {
-				if (args.length != 1) {
-					System.err.println("missing URL argument");
-					System.exit(1);
-				}
-				URI uri = URI.create(args[0]);
-				HttpClient client = HttpClient.newBuilder()
-						.version(HttpClient.Version.HTTP_2)
-						.build();
-				HttpRequest request = HttpRequest.newBuilder()
-						.uri(uri)
-						.timeout(Duration.ofSeconds(1))
-						.build();
-				HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-				System.out.print(response.body());
+	public static void main(String[] args) throws Exception {
+		if (args.length != 1) {
+			System.err.println("missing URL argument");
+			System.exit(1);
 		}
+		URI uri = URI.create(args[0]);
+		HttpClient client = HttpClient.newBuilder()
+			.version(HttpClient.Version.HTTP_2)
+			.build();
+		HttpRequest request = HttpRequest.newBuilder()
+			.uri(uri)
+			.timeout(Duration.ofSeconds(1))
+			.build();
+		HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+		System.out.print(response.body());
+	}
 }
-
 
